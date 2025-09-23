4c71e9e8a5cbfd65266ebaa7f366034425d8e5fe
YB-Taekwon <loundo001@gmail.com>
2025-09-23 19:26:33 +0900

    init commit

---
+++ src/main/java/com/ian/selectshop/SelectShopApplication.java
@@ -0,0 +1,13 @@
+package com.ian.selectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectShopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectShopApplication.class, args);
+    }
+
+}
